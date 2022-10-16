ddfc8ddeaa2f54c2f7c5a4d71ad5edc71983a2b8
Nasuf MUTLU <nasufmutlu@hotmail.com>
2022-10-16 17:57:19 +0300

    .

--- MvcBlog/Program.cs
+++ MvcBlog/Program.cs
@@ -2,7 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
+var mvcBuilder = builder.Services.AddRazorPages();
+if (builder.Environment.IsDevelopment())
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
