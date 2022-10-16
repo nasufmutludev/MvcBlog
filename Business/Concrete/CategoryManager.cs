243162bf90e62adb9b2e8fccbd63f2c2f04a4f6c
Nasuf MUTLU <nasufmutlu@hotmail.com>
2022-10-16 16:40:40 +0300

    .

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Repositories
+{
+    public class CategoryManager : ICategoryService
+    {
+        private ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public void AddCategory(Category category)
+        {
+            _categoryDal.Add(category);
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return _categoryDal.Get(x => x.Id == id);
+        }
+    }
+}
