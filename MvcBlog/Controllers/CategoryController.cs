6db5adb6abbfd734e6e4a0c159b3eb4d8ef639ca
Nasuf MUTLU <nasufmutlu@hotmail.com>
2022-10-16 17:26:59 +0300

    .

---
+++ MvcBlog/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Concrete.EntityFramework;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager _categoryManager=new CategoryManager(new EfCategoryDal());
+
+        public IActionResult Index()
+        {
+            var result = _categoryManager.ListAllCategory();
+            return View(result);
+        }
+    }
+}
