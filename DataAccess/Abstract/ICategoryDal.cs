1c9fac600e341aed6a6a2685cabd617d87b54335
Nasuf MUTLU <nasufmutlu@hotmail.com>
2022-10-16 16:17:03 +0300

    .

---
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> ListAllCategory();
+        void AddCategory(Category category);
+        void RemoveCategory(Category category);
+        void UpdateCategory(Category category);
+    }
+}
